77d331c7b764084b5549ed422419e501e6851cfc
Masi <51009658+MasiMasinga@users.noreply.github.com>
2023-10-14 22:21:39 +0200

    feat - add form validation for login and registration form

--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
+// CSS
 import "./assets/css/base.css";
 import "./assets/css/main.css";
 
+// Pinia
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
-import App from "./App.vue";
+// Vue Router
 import router from "./router";
+
+// Vee Validate
+import VeeValidatePlugin from "./includes/validation";
+
+// Components
+import App from "./App.vue";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VeeValidatePlugin);
 
 app.mount("#app");
