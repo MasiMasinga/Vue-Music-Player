f1da8ed65b8633ca981019f7e0d3622cb7ce5dd3
Masi <51009658+MasiMasinga@users.noreply.github.com>
2023-10-15 18:42:09 +0200

    fix - remove firebase api keys

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -4,12 +4,12 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCSdFlbhN6iR4AgkJCj03TPrPvt5AGrMQ4",
-    authDomain: "musicplayer-be41e.firebaseapp.com",
-    projectId: "musicplayer-be41e",
-    storageBucket: "musicplayer-be41e.appspot.com",
-    messagingSenderId: "546326252769",
-    appId: "1:546326252769:web:b07204cd84ba83541e7ba0",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
 };
 
 // Initialize Firebase
