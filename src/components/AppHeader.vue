264c1b9005dfe1b24d63c412c1ca7c6f53c52ae8
Masi <51009658+MasiMasinga@users.noreply.github.com>
2023-10-16 02:33:06 +0200

    feat - add routing to manage page

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,24 +1,25 @@
 <template>
     <header id="header" class="bg-gray-700">
         <nav class="container mx-auto flex justify-start items-center py-5 px-4">
-            <router-link class="text-white font-bold uppercase text-2xl mr-4" to="/"
-                >Music</router-link
-            >
+            <router-link class="text-white font-bold uppercase text-2xl mr-4" exact-active-class="no-active"
+                :to="{ name: 'Home' }">
+                Music
+            </router-link>
             <div class="flex flex-grow items-center">
                 <ul class="flex flex-row mt-1">
                     <li>
-                        <router-link class="px-2 text-white" to="/about"> About </router-link>
+                        <router-link class="px-2 text-white" :to="{ name: 'About' }">
+                            About
+                        </router-link>
                     </li>
                     <li v-if="!userStore.userLoggedIn">
-                        <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
-                            >Login / Register</a
-                        >
+                        <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal">Login / Register</a>
                     </li>
                     <template v-else>
                         <li>
-                            <router-link class="px-2 text-white" to="/manage">Manage</router-link>
+                            <router-link class="px-2 text-white" :to="{ name: 'Manage' }">Manage</router-link>
                         </li>
-                        <li @click.prevent="userStore.signOut">
+                        <li @click.prevent="signOut">
                             <a class="px-2 text-white" href="#">Logout</a>
                         </li>
                     </template>
@@ -42,6 +43,13 @@ export default {
         toggleAuthModal() {
             this.modalStore.isOpen = !this.modalStore.isOpen;
             console.log(this.modalStore.isOpen);
+        },
+        signOut() {
+            this.userStore.signOut();
+
+            if (this.$route.meta.requiresAuth) {
+                this.$router.push({ name: "home" });
+            }
         },
     },
 };
