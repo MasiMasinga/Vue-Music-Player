a1d39986bee73e9033cff737214fece40c18d658
Masi <51009658+MasiMasinga@users.noreply.github.com>
2023-10-29 01:55:28 +0200

    api keys

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -5,12 +5,12 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
+    apiKey: "AIzaSyCSdFlbhN6iR4AgkJCj03TPrPvt5AGrMQ4",
+    authDomain: "musicplayer-be41e.firebaseapp.com",
+    projectId: "musicplayer-be41e",
+    storageBucket: "musicplayer-be41e.appspot.com",
+    messagingSenderId: "546326252769",
+    appId: "1:546326252769:web:b07204cd84ba83541e7ba0",
 };
 
 // Initialize Firebase
